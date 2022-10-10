be65e305784d12ff7ff17827de64bf04a615c45b
a-ok123 <a-ok123@gmail.com>
2022-10-10 17:57:58 -0400

    Fixes

--- app/models/api_key.py
+++ app/models/api_key.py
@@ -1,11 +1,10 @@
 from typing import TYPE_CHECKING
-from datetime import datetime, timedelta
+from datetime import datetime#, timedelta
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
-from app.core.config import settings
 
 if TYPE_CHECKING:
     from .user import User  # noqa: F401
@@ -17,6 +16,6 @@ class ApiKey(Base):
     can_nft = Column(Boolean, default=False)
     can_sense = Column(Boolean, default=False)
     can_cascade = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
     owner_id = Column(Integer, ForeignKey("user.id"))
     owner = relationship("User", back_populates="api_keys")
